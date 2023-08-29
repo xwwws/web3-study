f11f53e55ff9420952d52c4ca5a3196579e6c2a4
wz <17600820823@163.com>
2023-08-29 15:54:48 +0800

    demo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,12 @@ import { Button } from "antd";
 
 export const Home = () => {
   const { state } = useEth();
-  const { web3 } = state
+  const { web3, accounts } = state
   const onclick = async () => {
-    const res = await web3.eth.requestAccounts()
-    const account = res[0]
-    const banlance = await web3.eth.getBalance(account)
-    console.log(web3.utils.fromWei(banlance))
+    const balance = await web3.eth.getBalance(accounts[0])
+    console.log(balance)
   }
   return <>
-    <Button onClick={onclick} >123</Button>
+    <Button onClick={onclick}>123</Button>
   </>
 }
