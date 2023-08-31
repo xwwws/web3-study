225eda860cd9f2b54a61ab7a2ee24869af5efa96
wz <17600820823@163.com>
2023-08-31 13:54:32 +0800

    exchange 合约代码编写完成 修改 等待测试

--- truffle/contracts/Exchange.sol
+++ truffle/contracts/Exchange.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+// 源码遵循协议， MIT...
 pragma solidity >=0.4.22 <0.9.0;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./BDTToken.sol";
@@ -11,24 +12,30 @@ contract Exchange {
 
     address constant ETHER = address(0);
 
-    mapping(address => mapping(address => uint)) public tokens;
+    mapping(address=> mapping(address=>uint256)) public tokens;
 
     event Deposit(address token, address user, uint amount, uint balance);
 
-    constructor(address _feeAccount, uint _feePercent) {
+    constructor(address _feeAccount,uint256 _feePercent){
+
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
-    function depositEther() public payable {
+    //存以太币
+    function depositEther() payable public{
+        //msg.sender
+        //msg.value
         tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
         emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
     }
-
-    function depositToken(address _token, uint _amount) public {
-        require(_token != address(0));
-        require(BDTToken(_token).transferFrom(msg.sender, address(this), _amount));
+    //存其他货币
+    function depositToken(address _token,uint256 _amount) public{
+        require(_token!=ETHER);
+        //调用某个方法强行从你账户往当前交易所账户转钱
+        require(BDTToken(_token).transferFrom(msg.sender,address(this),_amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
-        emit Deposit(_token, msg.sender, _amount, tokens[ETHER][msg.sender]);
+
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
