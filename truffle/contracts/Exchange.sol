45ce1d50a08f823eeaa46b513731f6d3aa87bf29
wz <17600820823@163.com>
2023-09-01 18:22:10 +0800

    从交易所提取token到钱包

--- truffle/contracts/Exchange.sol
+++ truffle/contracts/Exchange.sol
@@ -6,37 +6,53 @@ import "./BDTToken.sol";
 
 contract Exchange {
 
-    address public feeAccount;
+  address public feeAccount;
 
-    uint public feePercent;
+  uint public feePercent;
 
-    address constant ETHER = address(0);
+  address constant ETHER = address(0);
 
-    mapping(address => mapping(address => uint256)) public tokens;
+  mapping(address => mapping(address => uint256)) public tokens;
 
-    event Deposit(address token, address user, uint amount, uint balance);
+  event Deposit(address token, address user, uint amount, uint balance);
+  event WithDraw(address token, address user, uint amount, uint balance);
 
-    constructor(address _feeAccount, uint256 _feePercent) {
-        feeAccount = _feeAccount;
-        feePercent = _feePercent;
-    }
+  constructor(address _feeAccount, uint256 _feePercent) {
+    feeAccount = _feeAccount;
+    feePercent = _feePercent;
+  }
 
-    //存以太币
-    function depositEther() payable public  {
-        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] + msg.value;
-        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
-    }
+  //存以太币
+  function depositEther() payable public {
+    tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] + msg.value;
+    emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+  }
 
+  //存其他货币
+  function depositToken(address _token, uint256 _amount) public {
+    require(_token != address(0));
+    //调用某个方法强行从你账户往当前交易所账户转钱
+    require(BDTToken(_token).transferFrom(msg.sender, address(this), _amount));
+    tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
 
-    //存其他货币
-    function depositToken(address _token, uint256 _amount) public {
-        require(_token != address(0));
-        //调用某个方法强行从你账户往当前交易所账户转钱
-        require(
-            BDTToken(_token).transferFrom(msg.sender, address(this), _amount)
-        );
-        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+    emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+  }
 
-        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
-    }
+  function withDrawEther(uint256 _amount) public  {
+    require(tokens[ETHER][msg.sender] > _amount);
+    tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] - _amount;
+    // payable  当前合约地址转到msg.sender
+    payable(msg.sender).tranfer(_amount);
+
+    emit WithDraw(ETHER,  msg.sender, _amount,  tokens[ETHER][msg.sender]);
+  }
+  // 提取token
+  function withDrawToken(address _token, uint256 _amount) public  {
+    require(_token != address(0));
+    require(tokens[_token][msg.sender] > _amount);
+    require(BDTToken(_token).transfer(msg.sender, _amount));
+    tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+    emit WithDraw(_token,  msg.sender, _amount,  tokens[_token][msg.sender]);
+  }
+
 }
