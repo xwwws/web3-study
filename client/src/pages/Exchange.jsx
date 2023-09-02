4c4ca4bc75b55cc5ef417b8d53774e59b4c5894b
wangzhen <17600820823@163.com>
2023-09-02 20:09:11 +0800

    feat: solidity  交易所 存款 取款完成  前端存取款未完成

--- client/src/pages/Exchange.jsx
+++ client/src/pages/Exchange.jsx
@@ -1,57 +1,63 @@
 import useEth from "../contexts/EthContext/useEth";
-import { Button, Form, Input, Select } from "antd";
-import React, { useState } from "react";
+import {Button, Form, Input, Select} from "antd";
+import React, {useCallback, useEffect, useState} from "react";
 import styled from "styled-components";
 import Web3 from "web3";
+
+const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 const AccountBox = styled.div`
   padding-top: 20px;
   margin: 0 auto;
   width: 300px;
 `
 export const Exchange = () => {
-  const { state:{ web3, contract: {BDTToken,Exchange}, accounts } } = useEth();
+  const {state: {web3, contract: {BDTToken, Exchange}, accounts}} = useEth();
   const [form] = Form.useForm()
-  const [fromBalance, setFromBalance] = useState('')
-  const rules = [{ required: true, message: '必填' }]
-  const handleSubmit = async (val) => {
-    const s = 20
+  const rules = [{required: true, message: '必填'}]
+  const [tokens, SetTokens] = useState('0')
+  const handleDeposit = useCallback(async () => {
+    const values = await form.validateFields()
+    console.log(Exchange)
     await Exchange.methods.depositEther().send({
-      from: val.from,
-      value: Web3.utils.toWei(val.count.toString(),"ether")
+      from: values.from,
+      value: Web3.utils.toWei(values.count.toString(), "ether")
     })
-    console.log('以太坊存入成功')
-    // BDTToken.methods.transfer(val.to,web3.utils.toWei(val.count)).send({from: val.from})
-  }
-  const handleAccountChange = async (val) => {
-    // const banlance = await BDTToken.methods.balanceOf(val).call()
-    console.log(await web3.eth.getBalance(val))
-    // setFromBalance(web3.utils.fromWei(banlance.toString(), "ether"))
-  }
+  }, [form, Exchange])
+  const handleWithDraw = useCallback(async () => {
+    const values = await form.validateFields()
+    try {
+      await Exchange.methods.withDrawEther(Web3.utils.toWei(values.count.toString(), "ether")).send({
+        from: values.from
+      })
+    } catch (err) {
+      console.log(err.stack)
+      const stack = JSON.parse(err.stack)
+      console.log(stack)
+    }
+
+  }, [form, Exchange])
+  useEffect(() => {
+    (async () => {
+      const res = await Exchange.methods.tokens(ETHER_ADDRESS, accounts[0]).call()
+      console.log(Web3.utils.fromWei(res, "ether"))
+    })()
+  }, [form, Exchange])
   return <>
     <AccountBox>
-      <Form onFinish={ handleSubmit } form={ form }>
-        <Form.Item label={ `account from` } name={ `from` } rules={ rules }>
+      <Form form={form}>
+        <Form.Item label={`account from`} name={`from`} rules={rules}>
           <Select
-            options={ accounts?.map(item => ({ label: item, value: item })) }
-            onChange={ handleAccountChange }
+            options={accounts?.map(item => ({label: item, value: item}))}
           />
         </Form.Item>
-        {
-          fromBalance &&
-          <Form.Item label={ `from balance` }>
-            { fromBalance }
-          </Form.Item>
-        }
-        <Form.Item label={ `token count` } name={ `count` } rules={ rules }>
+        <Form.Item label={`token count`} name={`count`} rules={rules}>
           <Input></Input>
         </Form.Item>
-        {/*<Form.Item label={ `account to` } name={ `to` } rules={ rules }>*/}
-        {/*  <Select*/}
-        {/*    options={ accounts?.map(item => ({ label: item, value: item })) }*/}
-        {/*  />*/}
-        {/*</Form.Item>*/}
-        <Form.Item wrapperCol={ { offset: 10 } }>
-          <Button htmlType={ "submit" } type={ "primary" }>deposit</Button>
+        <Form.Item wrapperCol={{offset: 10}}>
+          <Button type={"primary"} onClick={handleDeposit}>deposit</Button>
+        </Form.Item>
+        <Form.Item wrapperCol={{offset: 10}}>
+          <Button type={"primary"} onClick={handleWithDraw}>withDraw</Button>
         </Form.Item>
       </Form>
     </AccountBox>
--- truffle/scripts/Exchange.js
+++ truffle/scripts/Exchange.js
@@ -6,17 +6,74 @@
 */
 
 const Exchange = artifacts.require("Exchange");
+const BDTToken = artifacts.require("BDTToken");
 const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 const toWei = number => web3.utils.toWei(number.toString(), 'ether')
 const fromWei = number => web3.utils.fromWei(number.toString(), 'ether')
 module.exports = async function (callback) {
   const exchange = await Exchange.deployed();
+  const BDT = await BDTToken.deployed();
   const accounts = await web3.eth.getAccounts()
-  await exchange.depositEther({
-    from:accounts[0],
-    value:toWei(10)
+  const acc0 = accounts[0]
+
+
+
+// console.group('ETHER')
+
+//   console.log('acc0 交易所中 user ETH,  余额：', fromWei(await exchange.tokens(ETHER_ADDRESS,acc0)))
+//   await exchange.depositEther({
+//     from:acc0,
+//     value:toWei(10)
+//   })
+//   console.log('acc0 给交易所中存入 ETH 完成')
+
+//   console.log('acc0 交易所中 user ETH,  余额：', fromWei(await exchange.tokens(ETHER_ADDRESS,acc0)))
+
+
+//   await exchange.withDrawEther(toWei(10),{
+//     from: acc0,
+//   })
+//   console.log('acc0 从交易所中提取 ETH 完成')
+
+
+//   console.log('acc0 交易所中 user ETH,  余额：', fromWei(await exchange.tokens(ETHER_ADDRESS,acc0)))
+
+// console.groupEnd('ETHER')
+
+  console.group('token')
+  await BDT.approve(exchange.address,toWei(100000),{
+    from: acc0
   })
-  const acc0Balance = await exchange.tokens(ETHER_ADDRESS,accounts[0])
-  console.log(fromWei(acc0Balance))
+  console.log('授权额度', fromWei(await BDT.allowance(exchange.address, acc0)))
+
+  await exchange.depositToken(BDT.address, toWei(100000),{
+    from: acc0
+  })
+  const exchangeBDTBalance = await exchange.tokens(BDT.address, acc0,{from: acc0})
+  console.log('交易所中BDT余额:', fromWei(exchangeBDTBalance))
+  
+  console.log('授权额度', fromWei(await BDT.allowance(exchange.address, acc0)))
+
+
+
+  console.groupEnd('token')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   callback();
 };
