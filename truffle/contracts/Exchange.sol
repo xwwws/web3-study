1b670cabc41cb93dabe603cf035017348104cccb
wangzhen <17600820823@163.com>
2023-09-03 21:54:19 +0800

    feat: 合约  交易合约代码编写完成

--- truffle/contracts/Exchange.sol
+++ truffle/contracts/Exchange.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 // 源码遵循协议， MIT...
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "./BDTToken.sol";
 
 contract Exchange {
     address public feeAccount;
 
-    uint public feePercent;
+    uint256 public feePercent;
+
+    uint256 public feeRate;
 
     address constant ETHER = address(0);
 
@@ -19,7 +21,17 @@ contract Exchange {
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+        feeRate = feePercent / 100;
     }
+
+    // 查询余额
+    function balanceOf(
+        address _token,
+        address _account
+    ) public view returns (uint256) {
+        return tokens[_token][_account];
+    }
+
     //存以太币
     function depositEther() public payable {
         tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] + msg.value;
@@ -54,5 +66,145 @@ contract Exchange {
         require(BDTToken(_token).transfer(msg.sender, _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
         emit WithDraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    /**
+		订单相关
+	 */
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenPay;
+        uint256 amountPay;
+        uint256 timestamp;
+    }
+    mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancel;
+    mapping(uint256 => bool) public orderFill;
+    uint256 public orderCount;
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenPay,
+        uint256 amountPay,
+        uint256 timestamp
+    );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenPay,
+        uint256 amountPay,
+        uint256 timestamp
+    );
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenPay,
+        uint256 amountPay,
+        uint256 timestamp
+    );
+
+    function payGas(address tokenAddress, address user, uint256 fee) private {
+        tokens[tokenAddress][feeAccount] =
+            tokens[tokenAddress][feeAccount] +
+            fee;
+        tokens[tokenAddress][user] = tokens[tokenAddress][user] - fee;
+    }
+
+    // makeOrder
+
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenPay,
+        uint256 _amountPay
+    ) public {
+        uint256 fee = (_amountPay * feeRate) / 2;
+        require(tokens[_tokenPay][msg.sender] > (_amountPay + fee));
+        orderCount = orderCount + 1;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenPay,
+            _amountPay,
+            block.timestamp
+        );
+        // 付费
+        payGas(_tokenPay, msg.sender, fee);
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenPay,
+            _amountPay,
+            block.timestamp
+        );
+    }
+
+    // cancelOrder
+    function cancelOrder(uint256 _orderId) public {
+        _Order memory curOrder = orders[_orderId];
+        require(curOrder.id == _orderId);
+        orderCancel[_orderId] = true;
+        emit Cancel(
+            curOrder.id,
+            msg.sender,
+            curOrder.tokenGet,
+            curOrder.amountGet,
+            curOrder.tokenPay,
+            curOrder.amountPay,
+            block.timestamp
+        );
+    }
+
+    // fillOrder
+
+    function fillOrder(uint256 _orderId) public {
+        _Order memory curOrder = orders[_orderId];
+        require(curOrder.id == _orderId);
+        uint256 fee = (curOrder.amountGet * feeRate) / 2;
+		require(tokens[curOrder.tokenGet][msg.sender] > (curOrder.amountGet + fee));
+        orderFill[_orderId] = true;
+        // uint256 gas =
+        // 账户余额更换  && 收取小费（填充订单的人付费）
+        // 发起交易账户 余额处理
+        tokens[curOrder.tokenPay][curOrder.user] =
+            tokens[curOrder.tokenPay][curOrder.user] -
+            curOrder.amountPay;
+        tokens[curOrder.tokenGet][curOrder.user] =
+            tokens[curOrder.tokenGet][curOrder.user] +
+            curOrder.amountGet;
+
+        // 填充交易账户 余额处理
+        tokens[curOrder.tokenPay][msg.sender] =
+            tokens[curOrder.tokenPay][msg.sender] +
+            curOrder.amountPay;
+        tokens[curOrder.tokenGet][msg.sender] =
+            tokens[curOrder.tokenGet][msg.sender] +
+            curOrder.amountGet;
+
+		// 处理手续费
+		payGas(curOrder.tokenGet, msg.sender, fee);
+
+        emit Trade(
+            curOrder.id,
+            curOrder.user,
+            curOrder.tokenGet,
+            curOrder.amountGet,
+            curOrder.tokenPay,
+            curOrder.amountPay,
+            block.timestamp
+        );
     }
 }
--- truffle/migrations/6_deploy_Exchange.js
+++ truffle/migrations/6_deploy_Exchange.js
@@ -1,5 +1,6 @@
 const Exchange = artifacts.require("Exchange");
 
 module.exports = function (deployer) {
-  deployer.deploy(Exchange, `0xde1A85db11327f4F9A4Fcc98096c4e27632Ba6Aa`, 6);
+  // d80914860a321b81a466bdc30d83b9b690d6666d77b3ce7494cc9863a21c4775
+  deployer.deploy(Exchange, `0xef292cee1fc47e2964be539060ad454d380e2a83`, 6);
 };
