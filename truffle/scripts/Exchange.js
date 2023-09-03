365f3c71d8c4ba282f6b9c331ce9dd0ebe4e5b02
wangzhen <17600820823@163.com>
2023-09-03 20:07:03 +0800

    feat: solidity  交易所 存款 取款完成  测试用例完成

--- truffle/scripts/Exchange.js
+++ truffle/scripts/Exchange.js
@@ -18,7 +18,7 @@ module.exports = async function (callback) {
 
 
 
-// console.group('ETHER')
+  console.group('------------------------ETHER---------------------------')
 
 //   console.log('acc0 交易所中 user ETH,  余额：', fromWei(await exchange.tokens(ETHER_ADDRESS,acc0)))
 //   await exchange.depositEther({
@@ -38,25 +38,50 @@ module.exports = async function (callback) {
 
 //   console.log('acc0 交易所中 user ETH,  余额：', fromWei(await exchange.tokens(ETHER_ADDRESS,acc0)))
 
-// console.groupEnd('ETHER')
+console.groupEnd('ETHER')
 
-  console.group('token')
-  await BDT.approve(exchange.address,toWei(100000),{
+  console.group('------------------------token---------------------------')
+  const logBalance = async (acc) => {
+    const walletBalance = [
+      {
+        name:'wallet BDT',
+        balance: fromWei(await BDT.balanceOf(acc))
+      },
+      {
+        name:'exchange BDT',
+         balance: fromWei(await exchange.tokens(BDT.address, acc))
+      }
+    ]
+    console.table(walletBalance,['name','balance'])
+  }
+  const logAlowance = async acc => {
+    console.log('授权额度', fromWei(await BDT.allowance(acc0,exchange.address )))
+  }
+  console.group('-----存入--token-----')
+
+  // 授权给交易所额度
+  await BDT.approve(exchange.address,toWei(100),{
     from: acc0
   })
-  console.log('授权额度', fromWei(await BDT.allowance(acc0,ETHER_ADDRESS )))
+  await logAlowance(acc0)
 
-  await exchange.depositToken(BDT.address, toWei(100000),{
+  // 将BDT转移到交易所
+  await exchange.depositToken(BDT.address, toWei(100),{
     from: acc0
   })
-  const exchangeBDTBalance = await exchange.tokens(BDT.address, acc0,{from: acc0})
-  console.log('交易所中BDT余额:', fromWei(exchangeBDTBalance))
-  
-  console.log('授权额度', fromWei(await BDT.allowance(ETHER_ADDRESS, acc0)))
+
+  await logBalance(acc0)
+  console.groupEnd()
 
 
+  console.group('-----提取--token-----')
+  // 将交易所中的BDT转移到钱包（BDT）
+  await exchange.withDrawToken(BDT.address, toWei(10),{
+    from: acc0
+  })
+  await logBalance(acc0)
 
-  console.groupEnd('token')
+  console.groupEnd()
 
 
 
